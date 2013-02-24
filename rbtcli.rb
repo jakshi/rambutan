ccbda8097e837e6b2309db6aab2300767cdb3da7
Konstantin Lysenko <konstantin@atalanta-systems.com>
2013-02-24 23:51:16 +0700

    Remove: part of unneeded code for parsing commandline options.
      * Because I don't need commandline options now, only command line prompt.

--- rbtcli.rb
+++ rbtcli.rb
@@ -44,6 +44,7 @@ require "prompt"
 require "mixlib/config"
 require "mixlib/cli"
 require "rambutan/version.rb"
+reqyure "rambutan/rbtcli_prompt.rb"
 
 extend Prompt::DSL
 
@@ -53,21 +54,25 @@ param :timer_limit, "Time when to stop timer", %w(25:00)
 
 desc "Start timer"
 command "start" do |timer_limit|
+#  self.start
   puts "Timer started at 00:00"
 end
 
 desc "Stop timer"
 command "stop" do
+# self.stop
   puts "Timer stopped at 25:00"
 end
 
 desc "Reset timer"
 command "reset" do
+# self.reset
   puts "Timer reseted to 00:00"
 end
 
 desc "Check timer"
 command "check" do
+# self.check
   puts "Timer is at 00:01"
 end
 
@@ -78,104 +83,21 @@ command "version" do
   puts "rambutan #{Rambutan::VERSION} (c) 2012-2013 Konstantin Lysenko"
 end
 
+
 Prompt::Console.start
 
 =begin
 module Rambutan
-
-  class CLIOptions
-    include Mixlib::CLI
-
-    option :start_timer,
-      :short => "st",
-      :long => "start",
-      :description => "Starts timer"
-
-    option :stop_timer,
-      :short => "sp",
-      :long => "stop",
-      :description => "Stops timer"
-
-    option :reset_timer,
-      :short => "r",
-      :long => "reset",
-      :description => "Reset timer"
-
-    option :check_timer,
-      :short => "c",
-      :long => "check",
-      :description => "Check timer value and print it"
-
-    option :version,
-      :short => "-v",
-      :long => "--version",
-      :description => "Print version info"
-  
-    option :help,
-      :short => "-h",
-      :long => "--help",
-      :description => "Show this message",
-      :on => :tail,
-      :boolean => true,
-      :show_options => true,
-      :exit => 0
-  end
-  
+ 
   class CLI
     def run
-      # Proccess command line parameters
-      ## If no arguments - then let's print help
-    if ARGV.empty?
-      puts "Well, this's embarassing, but you need to specify an option! Please read help:"
-      ARGV << "-h"
-    end
-
-    cli = CLIOptions.new
-    banner = <<-eos
-rbtcli is an command line client for rambutan timer
-
-Usage:
-user@host:~$ ruby rbtcli
-[00:00]> start 25:00
-Timer started at 00:00
-[00:01]>
-...
-[03:12]> stop
-Timer stoped at 03:12
-[03:12]>
-...
-[12:23]> check
-Timer is at 12:23
-[12:24]>
-...
-[21:11]> reset
-Timer resetted to 00:00
-[00:00]>
-...
-[25:00]> exit
-user@host:~$
-eos
-    cli.banner=banner
-    ## Parse options
-    begin
-      cli.parse_options
-    rescue OptionParser::InvalidOption
-      # if we have wrong option, print help.
-      puts "Well, this's embarassing, but wrong option! Please read help:"
-      ARGV.clear << "-h"
-      retry
-    end
     cli.config.each do |opt,value|
       if value
         case opt
           when :start_timer
-            self.start
           when :stop_timer
-            self.stop
           when :reset_timer
-            self.reset
           when :check_timer
-            self.check
           when :version
           puts "rambutan #{Rambutan::VERSION} (c) 2012-2013 Konstantin Lysenko"
         end
